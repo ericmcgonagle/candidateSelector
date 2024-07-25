f220c664b4b67a1688026e1ffc70d5f9c6446635
Siddharth Venkatraman <sid.venkatraman17@gmail.com>
2024-07-24 23:21:13 -0500

    Card Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { CandidateCardPageComponent } from './candidate-card-page/candidate-card-page.component';
+import { CardComponent } from './card/card.component'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LandingPageComponent, CandidateCardPageComponent],
+  imports: [RouterOutlet, LandingPageComponent, CandidateCardPageComponent, CardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { CandidateCardPageComponent } from './candidate-card-page/candidate-card-page.component';
+import { CardComponent } from './card/card.component'
 
 export const routes: Routes = [
     { path: '', component: LandingPageComponent },
-    { path: '', component: CandidateCardPageComponent }
+    { path: '', component: CandidateCardPageComponent },
+    { path: '', component: CardComponent }
 ];
