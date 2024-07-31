c893a9a52a173cd90c4767a0fc6b319068a01bff
ermcgonagle <ermcgonagle>
2024-07-30 22:56:49 -0500

    added javascript submit button form functionality

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,28 +1,42 @@
 import { Component, Renderer2, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 declare function greet(): void;
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.css'
 })
 export class LandingPageComponent {
 
-  // constructor() {
-  //   greet(   );
-  // }
+  username: string = '';
 
-  // constructor(private renderer: Renderer2) {}
+  // private scriptLoaded = false;
 
+
+  // constructor(private renderer: Renderer2) { }
 
   // ngOnInit() {
+  //   this.loadScript();
+  // }
+
+  // private loadScript() {
   //   const script = this.renderer.createElement('script');
   //   script.src = 'assets/example.js';
   //   script.onload = () => {
-  //     greet();
+  //     this.scriptLoaded = true;
   //   };
   //   this.renderer.appendChild(document.body, script);
+  // }
+
+  // onSignInClick() {
+  //   console.log("CLICKED");
+  //   if (this.scriptLoaded) {
+  //     (window as any).greet();
+  //   } else {
+  //     console.error("Script not loaded yet");
+  //   }
   // }
 }
--- src/assets/example.js
+++ src/assets/example.js
@@ -1,3 +1,16 @@
-function greet(){
-    alert("Hello");
- }
+function greet() {
+  alert("Hello");
+}
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  const usernameInput = document.getElementById('username');
+  const submitButton = document.querySelector('.submit-btn');
+
+  if (usernameInput && submitButton) {
+    submitButton.addEventListener('click', () => {
+      const inputValue = usernameInput.value;
+      console.log('Username input value:', inputValue);
+    });
+  }
+});
