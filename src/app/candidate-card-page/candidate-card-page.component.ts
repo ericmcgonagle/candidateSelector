902472528c5ccc6e49aad21ee3c35502615ee148
ermcgonagle <ermcgonagle>
2024-08-11 23:54:24 -0500

    Added two more hard coded app card components to candidate card page

--- src/app/candidate-card-page/candidate-card-page.component.ts
+++ src/app/candidate-card-page/candidate-card-page.component.ts
@@ -1,17 +1,23 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { CardComponent } from '../card/card.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-candidate-card-page',
   standalone: true,
-  imports: [CardComponent],
+  imports: [CardComponent, CommonModule],
   templateUrl: './candidate-card-page.component.html',
   styleUrl: './candidate-card-page.component.css'
 })
 export class CandidateCardPageComponent implements OnInit {
   memberName: string = '';
 
+  cards = [
+    { title: 'Sid Venkatraman', content: './assets/images/sidHeadshot.jpg' },
+    { title: 'Eric McGonagle', content: './assets/images/ericHeadshot.jpg' },
+    { title: 'Andrew Lin', content: './assets/images/linHeadshot.jpg' }
+  ];
   constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -9,4 +9,10 @@ import { Component, Input } from '@angular/core';
 export class CardComponent {
   @Input() title: string = '';
   @Input() content: string = '';
+
+  cards = [
+    { title: 'Sid Venkatraman', content: './assets/images/sidHeadshot.jpeg' },
+    { title: 'Eric McGonagle', content: './assets/images/ericHeadshot.jpeg' },
+    { title: 'Andrew Lin', content: './assets/images/linHeadshot.jpg' }
+  ];
 }
