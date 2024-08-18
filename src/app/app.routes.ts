9ffca8d35a93847eba95f792e90bc4fe728d6a70
ermcgonagle <ermcgonagle>
2024-08-18 15:34:55 -0500

    added styling changes to the upvote and downvote buttons

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { CandidateCardPageComponent } from './candidate-card-page/candidate-card-page.component';
 import { CardComponent } from './card/card.component'
+import { AdminPageComponent } from './admin-page/admin-page.component';
 
 export const routes: Routes = [
     { path: '', component: LandingPageComponent },
     { path: 'card-component', component: CardComponent },
     { path: 'candidate-card/:memberName', component: CandidateCardPageComponent },
+    { path: 'admin-page', component: AdminPageComponent}
 ];
