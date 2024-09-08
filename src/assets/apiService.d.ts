c46a1bbfd739495d275cf4d5a172454a2e5b9b8d
ermcgonagle <ermcgonagle>
2024-09-08 01:09:09 -0500

    adding foundation for calilng api

---
+++ src/assets/apiService.d.ts
@@ -0,0 +1,3 @@
+declare module '../../assets/apiService.js' {
+    export function fetchData(url: string): Promise<any>;
+}
---
+++ src/assets/apiService.js
@@ -0,0 +1,14 @@
+
+export async function fetchCandidates(url) {
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('Error fetching data: ', error);
+        throw error;
+    }
+}
